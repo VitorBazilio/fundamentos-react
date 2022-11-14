2fbf3a45b33dfcd72987b68e61f171e1c741320a
Vitor Bazilio <31036585+VitorBazilio@users.noreply.github.com>
2022-11-14 12:33:00 -0300

    Primeira aula de React
    Componente e fundamentos sobre como usar

---
+++ src/data/produtos.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {id: 1, nome: 'Brocolis', valor: 7.9}, 
+    {id: 2, nome: 'Rucula', valor: 6.9}, 
+    {id: 3, nome: 'Batata', valor: 5.4}, 
+    {id: 4, nome: 'Azeitona', valor: 8.2}, 
+    {id: 5, nome: 'Couve', valor: 8.4}, 
+    {id: 6, nome: 'Cenora', valor: 9.9}, 
+    {id: 7, nome: 'Tomate', valor: 7.5},
+]
